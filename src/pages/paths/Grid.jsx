530e151613019290ec75b5d3cd628c98225e7668
venkatk-git <venkatkumar.profiles@gmail.com>
2024-09-07 12:44:39 +0530

    Added Next & Previous Buttons

--- src/pages/paths/Grid.jsx
+++ src/pages/paths/Grid.jsx
@@ -1,35 +1,58 @@
+// Dependencies
 import styled from "styled-components";
+import React from "react";
 
+// Componenets
 import Box from "./Box";
+
+// Helpers & Contants
 import { findUniquePaths } from "../../utils/helpers";
 
 // eslint-disable-next-line react/prop-types
 function Grid({ grid, wayNumber }) {
+  const gridElement = React.useRef(null);
+
+  React.useLayoutEffect(() => {
+    if (!gridElement.current) return;
+
+    const gridRows = gridElement.current.children;
+
+    Array.from(gridRows).forEach((row, rowIndex) => {
+      const rowBoxes = row.children;
+
+      Array.from(rowBoxes).forEach((box, colIndex) => {
+        if (contains(rowIndex, colIndex, wayNumber)) {
+          const redBox = <RedBox key={`${rowIndex}-${colIndex}-path`} />;
+        }
+      });
+    });
+  });
   function contains(x, y, way) {
+    // eslint-disable-next-line react/prop-types
     const m = grid.length;
+    // eslint-disable-next-line react/prop-types
     const n = grid[0].length;
     const paths = findUniquePaths(m, n);
     return paths[way].some(([px, py]) => px === x && py === y);
   }
 
   return (
-    <div>
+    <GridWrapper ref={gridElement}>
       {
         // eslint-disable-next-line react/prop-types
         grid.map((el, rowIndex) => (
           <RowWrapper key={rowIndex}>
             {el.map((el, colIndex) => {
-              if (contains(rowIndex, colIndex, wayNumber)) {
-                return <RedBox key={`${rowIndex}-${colIndex}`} />;
-              }
               return <Box key={`${rowIndex}-${colIndex}`} />;
             })}
           </RowWrapper>
         ))
       }
-    </div>
+    </GridWrapper>
   );
 }
+
+const GridWrapper = styled.div``;
 
 const RowWrapper = styled.div`
   display: flex;
--- src/pages/paths/PathsPage.jsx
+++ src/pages/paths/PathsPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Grid from "./Grid";
 import { findUniquePaths } from "../../utils/helpers";
 
@@ -15,13 +15,46 @@ function PathsPage({ grid }) {
 
   const [wayNumber, setWayNumber] = React.useState(0);
 
+  const handleNextWay = () => {
+    setWayNumber((prev) => {
+      if (prev == uniqueWays - 1) return prev;
+
+      return (prev += 1);
+    });
+  };
+
+  const handlePrevWay = () => {
+    setWayNumber((prev) => {
+      if (prev == 0) return prev;
+
+      return (prev -= 1);
+    });
+  };
+
   return (
     <Wrapper>
       <Span>{`${m} x ${n}`}</Span>
-      <Grid grid={grid} wayNumber={wayNumber} />
-      <Button onClick={() => setWayNumber((prev) => (prev += 1))}>
-        {wayNumber < uniqueWays ? "+" : "-"}
-      </Button>
+      <GridWrapper>
+        <Grid grid={grid} wayNumber={wayNumber} />
+        <ButtonWrapper>
+          <Button
+            onClick={handleNextWay}
+            disabledbutton={wayNumber === uniqueWays - 1}
+          >
+            {"👉"}
+          </Button>
+          <Button onClick={handlePrevWay} disabledbutton={wayNumber === 0}>
+            {"👈"}
+          </Button>
+        </ButtonWrapper>
+      </GridWrapper>
+      <Span>
+        There are <b>{uniqueWays}</b> unique ways in{" "}
+        <b>
+          {m} x {n}
+        </b>{" "}
+        board
+      </Span>
     </Wrapper>
   );
 }
@@ -30,16 +63,57 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 24px;
+  align-items: center;
+`;
+
+const GridWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  align-items: center;
 `;
 
 const Span = styled.span`
-  font-weight: bold;
+  ${"" /* font-weight: bold; */}
   font-style: italic;
 `;
 
+const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DisabledButton = css`
+  opacity: 0;
+  cursor: default;
+
+  &:hover {
+    border-color: transparent;
+  }
+`;
+
 const Button = styled.div`
-  padding: 24px;
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
   cursor: pointer;
+  transition: border-color 0.25s;
+
+  &:hover {
+    border-color: #646cff;
+  }
+
+  &:focus,
+  &:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+
+  ${({ disabledbutton }) => disabledbutton && DisabledButton}
 `;
 
 export default PathsPage;
